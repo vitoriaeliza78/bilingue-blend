c8a2e96e90f57d0d01c8ad948688cb84f52fbc2f
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-02-15 12:52:46 -0300

    primeiro commit

---
+++ scriptt.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("caixaTxt-outro");
+  if (x.style.display === "none" || x.style.display === "") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+  
+
+
